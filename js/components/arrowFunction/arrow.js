879e117cab15e73fa8282be1e9e25014e4a20c1a
Ramas1985 <ramunasskarnulis@gmail.com>
2022-05-10 17:40:26 +0200

    arrow

--- js/components/arrowFunction/arrow.js
+++ js/components/arrowFunction/arrow.js
@@ -13,5 +13,5 @@ const pirmaRaide = text => text === '' ? '' : text[0];
 
 const paskutineRaide = text => text === '' ? '' : text[text.length - 1];
 
-const paskutineRaide = text => text === '' ? '' : text[Math.ceil(text.length / 2 - 1)];
+const vidurineRaide = text => text === '' ? '' : text[Math.ceil(text.length / 2 - 1)];
 
